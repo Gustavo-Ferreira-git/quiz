ae4c981a7b676d907f8a2eff8c2efb70ccb200c1
Gustavo Ferreira <gustavoferreirasantos1710@gmail.com>
2026-09-13 20:45:45 -0300

    versao 1

--- app/src/main/java/com/example/quiz/tela4.java
+++ app/src/main/java/com/example/quiz/tela4.java
@@ -15,7 +15,7 @@ public class tela4 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela4);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Tela1), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
